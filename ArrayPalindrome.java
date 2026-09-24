e351b032d0716ca022ccc1ccc6845b97dd57d502
R Abhilash <abhilash81354@gmail.com>
2026-09-24 21:09:46 +0530

    Add files via upload

---
+++ ArrayPalindrome.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+public class ArrayPalindrome {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 2, 1};
+        int start = 0;
+        int end = arr.length - 1;
+        boolean isPalindrome = true;
+        while (start < end){
+            if (arr[start] != arr[end]) {
+                isPalindrome = false;
+            }
+            start++;
+            end--;
+        }
+        if(isPalindrome) System.out.println("palindrome");
+        else System.out.println("not palindrome");
+    }
+}
